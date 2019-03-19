ef5a741aba908a34eb21f6425ab2d522fd8486ef
Sveg <36763190+Sveg@users.noreply.github.com>
2019-03-19 14:59:58 +0100

    Multi-tårde er nu lavet

--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Skat;
 
 namespace TCPServer
@@ -13,81 +14,24 @@ namespace TCPServer
         {
 
             IPAddress ip = IPAddress.Parse("127.0.0.1");
-
-
+            
             TcpListener serverSocket = new TcpListener(ip, 7000);
 
-
-            Afgift afgift = new Afgift();
-
-
             serverSocket.Start();
-            Console.WriteLine("Server started");
 
-            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-
-            Console.WriteLine("Server activated");
-
-            Stream ns = connectionSocket.GetStream();
-
-
-            StreamReader sr = new StreamReader(ns);
-            StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true;
             while (true)
             {
-                string message = sr.ReadLine();
-                string answer = "";
-                while (message != null && message != "")
-                {
+                Console.WriteLine("Server started");
 
-                    Console.WriteLine("Client: " + message);
-                    if (message.Contains("Elbil"))
-                    {
-                        var number = Regex.Match(message, @"\d+").Value;
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
 
-                        if (number != null)
-                        {
-                            int elbilafgift = Convert.ToInt32(number);
-                            
+                Console.WriteLine("Server activated");
 
-                            answer = "Afgift på elBil: " + afgift.ElBilAfgift(elbilafgift).ToString();
-                        }
-                        else
-                        {
-                            answer = "Angiv pris f.eks. 200000";
-                        }
-                    }
-                    else if (message.Contains("Personbil"))
-                    {
-                        var number = Regex.Match(message, @"\d+").Value;
+                TCPService service = new TCPService(connectionSocket);
 
-                        if (number != null)
-                        {
-                            int personbilafgift = Convert.ToInt32(number);
-                            
-
-                            answer = "Afgift på personBil: " + afgift.Bilafgift(personbilafgift).ToString();
-                        }
-                        else
-                        {
-                            answer = "Angiv pris f.eks. 200000";
-                        }
-                    }
-                    else
-                    {
-                        answer = "Angiv om det er PersonBil eller Elbil og prisen";
-                    }
-                    sw.WriteLine(answer);
-                    message = sr.ReadLine();
-
-                }
-
-                ns.Close();
-                connectionSocket.Close();
-
+                Task.Factory.StartNew(() => service.DoIt());
             }
-
+            
             serverSocket.Stop();
         }
         
---
+++ TCPServer/TCPServer/TCPService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using Skat;
+
+namespace TCPServer
+{
+    public class TCPService
+    {
+        private TcpClient connectionSocket;
+
+        public TCPService(TcpClient connectionSocket)
+        {
+            this.connectionSocket = connectionSocket;
+        }
+
+        internal void DoIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+
+            Afgift afgift = new Afgift();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+            while (true)
+            {
+                string message = sr.ReadLine();
+                string answer = "";
+                while (!string.IsNullOrEmpty(message))
+                {
+
+                    Console.WriteLine("Client: " + message);
+                    if (message.Contains("Elbil"))
+                    {
+                        var number = Regex.Match(message, @"\d+").Value;
+
+                        if (number != null)
+                        {
+                            int elbilafgift = Convert.ToInt32(number);
+
+
+                            answer = "Afgift på elbil: " + afgift.ElBilAfgift(elbilafgift).ToString();
+                        }
+                        else
+                        {
+                            answer = "Angiv pris f.eks. 200000";
+                        }
+                    }
+                    else if (message.Contains("Personbil"))
+                    {
+                        var number = Regex.Match(message, @"\d+").Value;
+
+                        if (number != null)
+                        {
+                            int personbilafgift = Convert.ToInt32(number);
+
+
+                            answer = "Afgift på personbil: " + afgift.Bilafgift(personbilafgift).ToString();
+                        }
+                        else
+                        {
+                            answer = "Angiv pris f.eks. 200000";
+                        }
+                    }
+                    else
+                    {
+                        answer = "Angiv om det er Personbil eller Elbil og prisen";
+                    }
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+
+                }
+
+                ns.Close();
+                connectionSocket.Close();
+
+            }
+        }
+    }
+}
